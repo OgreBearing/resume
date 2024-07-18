a28ac6a559feebbc044d02181bec39965bf753f5
brigeorge <brigeorge@gmail.com>
2024-07-18 03:49:37 +0000

    navbar almost working

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,6 +2,14 @@
 
 <script>
   import { page } from "$app/stores";
+  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome'
+  import { faBars, faThumbsUp, faXmark } from '@fortawesome/free-solid-svg-icons'
+  import { config } from '@fortawesome/fontawesome-svg-core'
+
+  import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+  config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+  let isMenuOpen = false;
 
   const paths = [
     { name: "Origin", url: "/" },
@@ -9,7 +17,56 @@
     { name: "Skills", url: "/skills" },
     { name: "Contact", url: "/contact" },
   ];
+
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+  }
 </script>
+<style>
+  .menu-transition {
+    transition: transform 0.3s ease-in-out;
+  }
+  .hidden-menu {
+    transform: translateX(100%);
+  }
+  .visible-menu {
+    transform: translateX(0);
+  }
+</style>
+
+<nav class="bg-white p-4 flex items-center justify-between font-caps">
+  <div class="text-gray-800 text-5xl font-semibold">Brian George</div>
+  <div class="hidden lg:flex space-x-4">  
+    {#each paths as path, index}
+      <div class="inline-block">
+        <a href={path.url} class="text-gray-800 text-2xl hover:bg-red-300" class:bg-gray-300={($page.url.pathname.includes(path.url) &&
+          path.url !== "/") ||
+          (path.url === "/" && $page.url.pathname === "/")}>
+              <div class="py-2 px-4 m-2 rounded-md bg-inherit">
+                {path.name}
+              </div></a>
+      </div>
+    {/each}
+  </div>
+  <div>
+    <button class="lg:hidden text-gray-800 text-2xl" on:click={toggleMenu}>
+      <p class:hidden={isMenuOpen}><FontAwesomeIcon icon={faBars} /></p>
+      <p class:hidden={!isMenuOpen}><FontAwesomeIcon icon={faXmark} /></p>
+    </button>
+  </div>
+</nav>
+
+<div class={`fixed top-0 right-0 w-2/3 h-full bg-blue-500 text-white p-4 menu-transition ${isMenuOpen ? 'visible-menu' : 'hidden-menu'}`}>
+  {#each paths as path, index}
+    <a href={path.url} class="block py-2 hover:bg-red-300" class:bg-gray-300={($page.url.pathname.includes(path.url) &&
+      path.url !== "/") ||
+      (path.url === "/" && $page.url.pathname === "/")}>
+          <div class="py-2 px-4 m-2 rounded-md bg-inherit">
+            {path.name}
+          </div></a>
+  {/each}
+</div>
+<!--
 <div class="container flex justify-between lg:justify-start font-caps">
   <div class="bg-white px-4 py-[0.65rem] flex-grow justify-start min-w-300">
     <div class="mx-auto text-gray-800 text-5xl font-semibold">Brian George</div>
@@ -39,5 +96,5 @@
       </div>
     {/each}
   </nav>
-</div>
+</div> -->
 
